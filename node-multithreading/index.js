c345efbc0eaa62f4558bda0bf4d3cb27c9e28dbf
Yonatan MK <yonatanmk@gmail.com>
2018-05-19 11:45:52 -0400

    passing data from child process to parent process

--- node-multithreading/index.js
+++ node-multithreading/index.js
@@ -7,17 +7,36 @@
 //   console.log('we got our catted file', stdout);
 // })
 
+// var spawn = require('child_process').spawn;
+//
+// if (process.argv[2] === 'child') {
+//   console.log('Im inside the child')
+// } else {
+//   // var child = spawn(process.execPath, [__filename, 'child'])
+//   // child.stdout.on('data', function(data) {
+//   //   console.log('from child', data.toString())
+//   // })
+//   var child = spawn(process.execPath, [__filename, 'child'], {
+//     stdio: [null, null, null],
+//   })
+//   // child.stdout.pipe(process.stdout)
+// }
+
 var spawn = require('child_process').spawn;
 
 if (process.argv[2] === 'child') {
-  console.log('Im inside the child')
+  var net = require('net');
+  var pipe = new net.Socket({ fd: 3 });
+  pipe.write('killMe');
 } else {
-  // var child = spawn(process.execPath, [__filename, 'child'])
-  // child.stdout.on('data', function(data) {
-  //   console.log('from child', data.toString())
-  // })
   var child = spawn(process.execPath, [__filename, 'child'], {
-    stdio: [null, null, null],
+    stdio: [null, null, null, 'pipe'],
+  });
+  child.stdio[3].on('data', data => {
+    if (data.toString() === 'killMe') {
+      console.log('RIP');
+      child.kill();
+    }
   })
   // child.stdout.pipe(process.stdout)
 }
